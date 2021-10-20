8bbb995646e7bcec36d608320ceac2143670a3b4
Edith Rincon <andre-lin@hotmail.es>
2021-10-20 07:22:53 -0500

    Agregar informacion de repaso JS

--- script/script.js
+++ script/script.js
@@ -6,5 +6,29 @@ console.log(nombrePlanetas)
 console.log(distancia)
 console.log(tamaño)
 
+let i=0;
+while(i<= nombrePlanetas.length - 1){
+    console.log(i)
+    console.log(nombrePlanetas[i]);
+    i=i+1
+}
+for (let index = 0; index < nombrePlanetas.length; index++) {
+    console.log(nombrePlanetas[index]);
+    }
+
+for(const key in nombrePlanetas){
+    console.log(nombrePlanetas[key])}
+
+for(const planeta of nombrePlanetas){
+    console.log(planeta)
+}
+for(const key in distancia){
+    if(distancia[key]==='5'){
+    console.log('Se encontró la distancia')}
+    else{
+    console.log('No se encontró la distancia')
+    }
+}
+
 
 
