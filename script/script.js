394446b9858c361f227071559164df9b1be876cd
Edith Rincon <andre-lin@hotmail.es>
2021-10-20 06:43:57 -0500

    ingreso al archivo script

---
+++ script/script.js
@@ -0,0 +1,10 @@
+let nombrePlanetas = Array ('Tierra','Marte','Jupiter')
+let distancia =[5,3,'67']
+const tamaño =[]
+
+console.log(nombrePlanetas)
+console.log(distancia)
+console.log(tamaño)
+
+
+
